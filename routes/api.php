cd190a2a2774384245b528862df874422b8c43db
Natthaphon Ditthaviboon <bug9best@gmail.com>
2025-09-08 02:37:25 +0700

    update route

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Http\Controllers\TaskCtrl;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::group(["prefix" => "tasks"], function () {
+    Route::get("getTasks", [TaskCtrl::class, "getTasks"]);
+    Route::get("getTaskById/{id}", [TaskCtrl::class, "getTaskById"]);
+    Route::post("createTask", [TaskCtrl::class, "createTask"]);
+    Route::put("updateTask/{id}", [TaskCtrl::class, "updateTask"]);
+    Route::delete("deleteTask/{id}", [TaskCtrl::class, "deleteTask"]);
+});
